6f97ed744942a79b4ba119a989b7f18ac2c55121
otherdcn <danny.c.ntwari@gmail.com>
2020-12-18 16:28:00 +0200

    Comment out some code in visible concern
    Certain functionalities are causing errors.

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,13 +1,17 @@
 module Visible
   extend ActiveSupport::Concern
 
-  included do
-    VALID_STATUSES = ['public', 'private', 'archived']
+  #included do
+  #  VALID_STATUSES = ['public', 'private', 'archived']
 
-    validates :status, in: VALID_STATUSES
-  end
-  
-  class_methods do
+  #  validates :status, in: VALID_STATUSES
+  #end
+ 
+  # <!-- <p>Our blog has <%= Article.public_count %> articles and counting!</p> -->
+  # The above should be in [app/views/articles/index.html.erb]
+  # But the error below always appears for [class_methods] and [included do] block above:
+  # ActionView::Template::Error (Unknown validator: 'InValidator'):
+  class_methods do #=> this is not working properly
     def public_count
       where(status: 'public').count
     end
